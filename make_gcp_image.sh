ba4c4bb8e6039d6286ae09b552a95ab45a763dad
Adam Olech <aolech@antmicro.com>
2021-07-29 15:38:59 +0200

    make_gcp_image: add variables

--- make_gcp_image.sh
+++ make_gcp_image.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+base_dir=`pwd`
+out_dir=$base_dir/output
+grub_path=`realpath grub-legacy`
+grub_path_build=$grub_path/build
+grub_s1=$grub_path_build/stage1/stage1
+grub_s2=$grub_path_build/stage2/stage2
+
 pecho() {
     printf '\x1b[1;32m>> \x1b[1;33m%s \x1b[0m%s\n' "${FUNCNAME[1]}" "$1"
 }
 
 make_grub() {
-    pecho "Configuring and compiling GRUB Legacy..."
+    pecho "configuring and compiling grub legacy..."
 
-    cd grub-legacy
-    mkdir -p build
-    ./autogen.sh
-    cd build
-    CFLAGS+=" -static -fno-strict-aliasing -fno-stack-protector" ../configure
-    make
+    mkdir -p $grub_path_build
+    cd $grub_path && ./autogen.sh
+    cd $grub_path_build CFLAGS+=" -static -fno-strict-aliasing -fno-stack-protector" ../configure
+    cd $grub_path_build && make
 
-    pecho "Done!"
+    pecho "copying stage binaries"
+
+    cp $grub_s1 $out_dir
+    cp $grub_s2 $out_dir
+
+    cd $base_dir
 }
 
-mkdir -p output
+mkdir -p $out_dir
 make_grub
