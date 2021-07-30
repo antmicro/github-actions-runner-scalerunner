4919cad2aabcac446e8905f9aa151758200d0f18
Adam Olech <aolech@antmicro.com>
2021-07-30 09:38:48 +0200

    make_gcp_image: add variables for disk files

--- make_gcp_image.sh
+++ make_gcp_image.sh
@@ -2,10 +2,14 @@
 
 base_dir=`pwd`
 out_dir=$base_dir/output
+
 grub_path=`realpath grub-legacy`
 grub_path_build=$grub_path/build
 grub_s1=$grub_path_build/stage1/stage1
 grub_s2=$grub_path_build/stage2/stage2
+
+fat_part=$out_dir/disk.fat
+raw_disk=$out_dir/disk.raw
 
 export PATH="$PATH:/usr/sbin"
 
@@ -23,13 +27,13 @@ make_grub() {
 
     pecho "copying stage binaries"
 
-    dd if=$grub_s1 of=$out_dir/disk.raw bs=512 count=1
-    dd if=$grub_s2 of=$out_dir/disk.raw bs=512 seek=1
+    dd if=$grub_s1 of=$raw_disk bs=512 count=1
+    dd if=$grub_s2 of=$raw_disk bs=512 seek=1
 
     pecho "clearing partition table (leaving MBR intact)"
-    dd if=/dev/zero of=$out_dir/disk.raw bs=1 count=64 seek=446 conv=notrunc
+    dd if=/dev/zero of=$raw_disk bs=1 count=64 seek=446 conv=notrunc
 
-    truncate -s 200M $out_dir/disk.raw
+    truncate -s 200M $raw_disk
 
     cd $base_dir
 }
@@ -38,24 +42,24 @@ prepare_bootdisk() {
     pecho "$(fdisk --version)"
 
     pecho "creating raw disk with bootable fat16 partition"
-    ( echo "n" ; echo p ; echo 1 ; echo "2048"; echo 409599; echo w; echo q ) | fdisk $out_dir/disk.raw
-    ( echo t; echo 6; echo ; echo w; echo q ) | fdisk $out_dir/disk.raw
-    ( echo a; echo w; echo q ) | fdisk $out_dir/disk.raw
+    ( echo "n" ; echo p ; echo 1 ; echo "2048"; echo 409599; echo w; echo q ) | fdisk $raw_disk
+    ( echo t; echo 6; echo ; echo w; echo q ) | fdisk $raw_disk
+    ( echo a; echo w; echo q ) | fdisk $raw_disk
 
     pecho "preparing fat16 partition"
-    truncate -s 199M $out_dir/disk.fat
-    mkfs.fat $out_dir/disk.fat
+    truncate -s 199M $fat_part
+    mkfs.fat $fat_part
 
     cp $base_dir/buildroot/output/build/linux-*/arch/x86/boot/bzImage $out_dir
 
     pecho "copying grub and kernel to fat16 partition"
-    mmd -i $out_dir/disk.fat ::boot
-    mmd -i $out_dir/disk.fat ::boot/grub
-    mcopy -i $out_dir/disk.fat $base_dir/menu.lst '::boot/grub/menu.lst'
-    mcopy -i $out_dir/disk.fat $out_dir/bzImage '::.'
+    mmd -i $fat_part ::boot
+    mmd -i $fat_part ::boot/grub
+    mcopy -i $fat_part $base_dir/menu.lst '::boot/grub/menu.lst'
+    mcopy -i $fat_part $out_dir/bzImage '::.'
 
     pecho "copying fat16 to raw disk" 
-    dd conv=notrunc if=$out_dir/disk.fat of=$out_dir/disk.raw bs=512 seek=2048
+    dd conv=notrunc if=$fat_part of=$raw_disk bs=512 seek=2048
 }
 
 mkdir -p $out_dir
