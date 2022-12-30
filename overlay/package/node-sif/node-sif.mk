b0863bec9f42031882f657fbc03cfe563aa0c9a0
Adam Olech <aolech@antmicro.com>
2022-12-30 13:19:11 +0100

    overlay: update node-sif to v0.1.5
    This also introduces architecture constraints and architecture-specific variants

--- overlay/package/node-sif/node-sif.mk
+++ overlay/package/node-sif/node-sif.mk
@@ -1,9 +1,15 @@
-NODE_SIF_VERSION = node-16-v0.1.4
+NODE_SIF_VERSION = node-16-v0.1.5
 NODE_SIF_SOURCE = $(NODE_SIF_VERSION).zip
 NODE_SIF_SITE = https://github.com/antmicro/github-actions-singularity-node/releases/download/$(NODE_SIF_VERSION)
 
+ifeq ($(BR2_aarch64),y)
+	NODE_SIF_VARIANT = buster-slim-arm64
+else ifeq ($(BR2_x86_64),y)
+	NODE_SIF_VARIANT = alpine3.14
+endif
+
 define NODE_SIF_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/node-16-alpine3.14.sif \
+	$(INSTALL) -D -m 0755 $(@D)/node-16-$(NODE_SIF_VARIANT).sif \
 		$(TARGET_DIR)/opt/sif/node.sif
 endef
 
