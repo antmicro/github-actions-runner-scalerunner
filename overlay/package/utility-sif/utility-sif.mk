105d9478722714d0e9fc8adb8900bd51878cbb33
Adam Olech <aolech@antmicro.com>
2022-12-30 13:35:58 +0100

    overlay: update utility-sif to v0.1.2-1

--- overlay/package/utility-sif/utility-sif.mk
+++ overlay/package/utility-sif/utility-sif.mk
@@ -1,9 +1,15 @@
-UTILITY_SIF_VERSION = v0.1.2
+UTILITY_SIF_VERSION = v0.1.2-1
 UTILITY_SIF_SOURCE = gha-utility-container-$(UTILITY_SIF_VERSION).zip
 UTILITY_SIF_SITE = https://github.com/antmicro/github-actions-singularity-utility-container/releases/download/$(UTILITY_SIF_VERSION)
 
+ifeq ($(BR2_aarch64),y)
+	UTILITY_SIF_IMAGE_NAME = image-arm64
+else ifeq ($(BR2_x86_64),y)
+	UTILITY_SIF_IMAGE_NAME = image
+endif
+
 define UTILITY_SIF_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/image.sif \
+	$(INSTALL) -D -m 0755 $(@D)/$(UTILITY_SIF_IMAGE_NAME).sif \
 		$(TARGET_DIR)/opt/sif/utility.sif
 endef
 
