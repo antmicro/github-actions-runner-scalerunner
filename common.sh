be774a5deca99490215b279e9ac92b31b79def38
Adam Olech <aolech@antmicro.com>
2023-01-04 10:48:06 +0100

    run_qemu.sh: adapt for arm64

--- common.sh
+++ common.sh
@@ -17,6 +17,30 @@ if [ ! -f "$bzimage" ]; then
     bzimage=$base_dir/buildroot/output/images/Image
 fi
 
+architecture_from_raw_disk() {
+    # Skip to MBR partition entry #1 (0x01BE == 446) and read partition type byte (0x01BE + 0x04 = 0x01C2 == 450).
+    local part_type="$(dd if=$1 bs=1 skip=450 count=1 status=none | od -A n -t x1 | sed 's/^ *//')"
+
+    # FAT partition volume is located in the Extended BIOS Parameter block.
+    # We detect the partition type from the MBR partition table -- 06 is FAT16, EF is EFI.
+    # An assumption is made that the EFI partition is formatted in FAT32 (even though the spec doesn't enforce this).
+    # In FAT12 and FAT16 the volume field is located at 0x02B (43) and in FAT32 at 0x47 (71).
+    local fat_label_offset="$([ "$part_type" == "06" ] && echo 43 || echo 71)"
+
+    # Skip partition table onto the first partition and read 11 bytes (max size) of the partition volume field.
+    # The field is padded with spaces so we use xargs to strip trailing spaces as well.
+    local fat_label="$(dd if=$1 bs=512 skip=2048 | dd bs=1 skip=$fat_label_offset count=11 status=none | xargs)"
+
+    # Provide compatibility for images with no label.
+    if [ "$fat_label" == "NO NAME" ]; then
+        echo "x86"
+        return
+    fi
+
+    # Strip the prefix (usually 'GHA_') to obtain the architecture.
+    echo $fat_label | cut -d_ -f2
+}
+
 __get_image_arch() {
     if [ -f "$bzimage" ]; then
         file -b $bzimage \
--- run_qemu.sh
+++ run_qemu.sh
@@ -13,15 +13,32 @@ if [ ! -f $test_disk ]; then
     fallocate -l 1G $test_disk
 fi
 
+image_arch="$(architecture_from_raw_disk $test_disk)"
+pecho "detected image architecture is $image_arch"
+
 pecho "removing $known_host_str"
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R $known_host_str
 
-qemu-system-x86_64 \
-    -drive if=none,id=scalerunner-boot-disk,format=raw,file=$test_disk \
-    -cpu host \
-    --enable-kvm \
-    -m 2G \
-    -nographic \
-    -nic user,model=virtio-net-pci,hostfwd=tcp::9022-:22 \
-    -device virtio-scsi-pci,id=scsi0 \
-    -device scsi-hd,drive=scalerunner-boot-disk,vendor=Google,product=PersistentDisk
+
+case $image_arch in
+    x86)
+        qemu-system-x86_64 \
+            -drive if=none,id=scalerunner-boot-disk,format=raw,file=$test_disk \
+            -cpu host \
+            --enable-kvm \
+            -m 2G \
+            -nographic \
+            -nic user,model=virtio-net-pci,hostfwd=tcp::9022-:22 \
+            -device virtio-scsi-pci,id=scsi0 \
+            -device scsi-hd,drive=scalerunner-boot-disk,vendor=Google,product=PersistentDisk;;
+    arm64)
+        qemu-system-aarch64 \
+            -nographic \
+            -M virt -m 2G \
+            -cpu max -smp 4 \
+            -device virtio-rng \
+            -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd \
+            -drive file=$test_disk,if=none,id=scalerunner-boot-disk,format=raw \
+            -device nvme,serial=deadbeef,drive=scalerunner-boot-disk \
+            -nic user,model=virtio-net-pci,hostfwd=tcp::9022-:22;;
+esac
